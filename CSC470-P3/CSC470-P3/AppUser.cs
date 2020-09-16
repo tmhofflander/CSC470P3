27e6747031e155b870d288551910a8a2e39dacab
Thaniel Hofflander <Thaniel.Hofflander@trojans.dsu.edu>
2020-09-15 20:11:22 -0500

    created AppUser class

---
+++ CSC470-P3/CSC470-P3/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSC470_P3
+{
+    public class AppUser
+    {
+        public int UserName;
+        public string Password;
+        public string FirstName;
+        public string LastName;
+        public string EmailAddress;
+        public bool IsAuthenticated;
+    }
+}
